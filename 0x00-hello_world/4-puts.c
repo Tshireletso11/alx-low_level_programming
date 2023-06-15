35e6f9d465885dc77b23db63c3db457d36f5fe65
Tshireletso11 <tshireletsobazzy@gmail.com>
2023-06-15 15:02:20 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	puts("programming is like building a multilingual puxxle\n");
+	return (0);
+}
