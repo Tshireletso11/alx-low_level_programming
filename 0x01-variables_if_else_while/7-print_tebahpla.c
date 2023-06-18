dd39ab88d011e86a8b79fa5f5d807be2f32fca40
Tshireletso11 <tshireletsobazzy@gmail.com>
2023-06-18 03:04:29 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - shows alphabets in reverse order
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char alphabets = 'z';
+
+	while (alphabets >= 'a')
+	{
+		putchar(alphabets);
+		alphabets--;
+	}
+		putchar('\n');
+		return (0);
+}
