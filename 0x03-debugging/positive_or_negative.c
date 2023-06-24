286e58e6c7f3cfaaaa83594dfc6d3101482b930b
Tshireletso11 <tshireletsobazzy@gmail.com>
2023-06-23 17:29:51 -0700

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - testes function that prints if integer is pos or neg
+ * Return: always 0 successful
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
