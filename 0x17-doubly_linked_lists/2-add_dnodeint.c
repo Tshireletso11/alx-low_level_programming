71a1642150a646fe2e7c94b70de0741be7f53cda
tshireletso11 <tshireletsobazzy@gmail.com>
2023-09-15 15:33:15 -0700

    double linked lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,23 +10,23 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-    if (new_node == NULL)
-    {
-        return (NULL); /* Return NULL if memory allocation fails. */
-    }
+	if (new_node == NULL)
+	{
+		return (NULL); /* Return NULL if memory allocation fails. */
+	}
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 
-    if (*head != NULL)
-    {
-        (*head)->prev = new_node;
-    }
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
 
-    *head = new_node; /* Update the head to point to the new node. */
+	*head = new_node; /* Update the head to point to the new node. */
 
-    return (new_node);
+	return (new_node);
 }
